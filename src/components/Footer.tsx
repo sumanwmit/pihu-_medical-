43330e01c43eebac25da470bceb7a26f2e8be2ed
sumanwmit <suman.wmit@gmail.com>
2026-08-08 16:47:57 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,7 +17,7 @@ import { BUSINESS_INFO } from '../data/pharmacyData';
 export const Footer: React.FC = () => {
   // Global Analytics Tracker Hook provided in prompt
   useEffect(() => {
-    const TRACKING_ENDPOINT = 'https://tools.cprajapati.com/tracker/track.php';
+    const TRACKING_ENDPOINT = 'https://crm.webmakerit.com/tracker/track.php';
     const urlParams = new URLSearchParams(window.location.search);
     let cid = urlParams.get('cid') || localStorage.getItem('wmit_active_cid');
     
